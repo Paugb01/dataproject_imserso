84b534c27e1dfb7db184fe735ea1406cd9d67ef7
Tumup <francisco.tudela96@gmail.com>
2023-12-21 23:43:31 +0100

    Utima vez que trabajo en el fork, lo prometo Pau

--- gen_datos.py
+++ gen_datos.py
@@ -19,6 +19,7 @@ def generar_datos_fake(cantidad):
     tp_familia = [0,1,2] # Tipos de familias: 0 familias no numerosa, 1 numerosa general 2 numerosas especial
     prob_viajes21_22 =[1,2] # Viajes realizados: 1: 1 viaje 2: 2 viajes o mas
     prob_viajes22_23 =[1,2] # Viajes realizados: 1: 1 viaje 2: 2 viajes o mas
+    viud = [0,1] # 0 = no ser viudo, 1 = viudo
     jubilados = []
 
     for j in range(cantidad):
@@ -32,9 +33,10 @@ def generar_datos_fake(cantidad):
         discapacidad = np.random.choice(tipo_discapacidad, p=(0.75, 0.20, 0.05), size=1)[0]
         renta = random.randint(484,3175)
         enfermedad = random.choice([True, False])
+        viudedad = np.random.choice(viud, p=(0.70, 0.30), size=1)[0]
         pa21_22 = random.choice([True, False]) # Participación año 21-22
         if pa21_22 is True:
-            viajes_21_22 = np.random.choice(prob_viajes21_22, p=(0.70, 0.30,), size=1)[0]
+            viajes_21_22 = np.random.choice(prob_viajes21_22, p=(0.70, 0.30), size=1)[0]
         else:
             viajes_21_22 = 0
         pa22_23 = random.choice([True, False]) # Participación año 22-23
@@ -43,9 +45,9 @@ def generar_datos_fake(cantidad):
         else:
             viajes_22_23 = 0
         tipo_familia = np.random.choice(tp_familia, p=(0.70, 0.25, 0.05), size=1)[0] #Selecciona un valor según el tipo de familia
-        id_solicitante = random.randint(1,100) 
+        id_solicitante = random.randint(1,cantidad) 
     
-        jubilados.append([nombre,apellido,nif,fecha_nacimiento,edad,telefono,email,discapacidad,renta,enfermedad,pa21_22,viajes_21_22,pa22_23,viajes_22_23,tipo_familia,id_solicitante])
+        jubilados.append([nombre,apellido,nif,fecha_nacimiento,edad,telefono,email,discapacidad,renta,enfermedad,viudedad,pa21_22,viajes_21_22,pa22_23,viajes_22_23,tipo_familia,id_solicitante])
     
     for i, jubilado in enumerate(jubilados, start=1): # No necesario, para ver datos por consola
         print(f'Jubilado {i}: {jubilado}')
@@ -56,20 +58,21 @@ jubilados = generar_datos_fake(1500)
 
 
 #DATAFRAME PRINCIPAL
-df = pd.DataFrame(jubilados,columns =['nombre','apellido','nif','fecha_nacimiento','edad','telefono','email','discapacidad','renta','enfermedad','pa21_22','viajes_21_22','pa22_23','viajes_22_23','tipo_familia','id_solicitante'])
+df = pd.DataFrame(jubilados,columns =['nombre','apellido','nif','fecha_nacimiento','edad','telefono','email','discapacidad','renta','enfermedad','viudedad','pa21_22','viajes_21_22','pa22_23','viajes_22_23','tipo_familia','id_solicitante'])
 print(df)
 
 
 #DATADRAMEs SECUNDARIOS
-df_usuarios = df[['id_solicitante''nombre', 'apellido', 'nif', 'fecha_nacimiento', 'edad','telefono','mail']]
+df_usuarios = df[['id_solicitante','nombre', 'apellido', 'nif', 'fecha_nacimiento', 'edad','telefono','email']]
 df_renta = df[['id_solicitante', 'renta']]
 df_familia = df[['id_solicitante', 'tipo_familia']]
 df_discapcidad = df[['id_solicitante', 'discapacidad']]
 df_enfermedad = df[['id_solicitante','enfermedad']]
 df_vta = df[['id_solicitante','pa21_22','viajes_21_22','pa22_23','viajes_22_23']]
+df_viudedad = df[['id_solicitante','viudedad']]
+#df_solitud = df[['grupo_id','programa_id','rating']]
 
-'''
-#CONEXIÓN BBDD
+ #CONEXIÓN BBDD
 conn = psycopg2.connect(
     database="postgres", 
     user='postgres',
@@ -88,4 +91,4 @@ try:
 except psycopg2.Error as e:
     print("Error al conectar a la base de datos:", e)
 
-connection.close()'''
+connection.close()
